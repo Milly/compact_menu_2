ef123dd70a4b9045f9266e8fd41bb461b3ea131a
Milly <milly.ca@gmail.com>
2007-02-20 05:56:12 +0000

    ja-JP locale に拡張機能についての詳細を追加。

--- defaults/preferences/compact.js
+++ defaults/preferences/compact.js
@@ -5,3 +5,4 @@ pref("compact.menu.showmenu.go", true);
 pref("compact.menu.showmenu.help", true);
 pref("compact.menu.showmenu.tasks", true);
 pref("compact.menu.showmenu.view", true);
+pref("extensions.{F9C645DA-6B91-494d-958C-4CA7B12129A5}.description", "chrome://compact/locale/global.properties");
