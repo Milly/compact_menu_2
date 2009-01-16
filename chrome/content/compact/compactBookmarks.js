0ed5c57221e1facec2c5ef19ec563e50e25c310c
Milly <milly.ca@gmail.com>
2009-01-16 11:53:39 +0000

    ブックマークの拡張を Javascript によるクローンで実装。 Compact Bookmarks ボタンを追加。

--- chrome/content/compact/compactBookmarks.js
+++ chrome/content/compact/compactBookmarks.js
@@ -1,80 +1,160 @@
 var CompactBookmarks = { __proto__: CompactMenu,
 
+// debug methods
+
+c_dump: function(msg) {
+  this.__proto__.c_dump.call(this, 'CompactBookmarks :: ' + msg);
+},
+
+// initialize methods
+
 init: function() {
+  this.c_dump('init');
   if (window.BookmarksMenuDNDObserver) {
-    var BookmarksMenuDNDObserver_mObservers = BookmarksMenuDNDObserver.__lookupGetter__('mObservers');
-    BookmarksMenuDNDObserver.__defineGetter__('mObservers', function() {
-      var observers = BookmarksMenuDNDObserver_mObservers.call(this);
-      var bm = document.getElementById('compact-bk-menubar');
-      if (bm) {
-        var popup = bm.getElementsByTagName('menupopup')[0];
-        if (popup) observers = [popup].concat(observers);
-      }
-      return observers;
-    });
+    this.initBookmarksFunctions_Fx2();
+  } else {
+    this.initBookmarksFunctions_Fx3();
   }
+  this.initBookmarksItems();
   this.addEventListener(window, 'unload', this, false);
   this.addEventListener(window, 'focus', this, false);
+},
+
+initBookmarksFunctions_Fx2: function() {
+  var BookmarksMenuDNDObserver_mObservers = BookmarksMenuDNDObserver.__lookupGetter__('mObservers');
+  BookmarksMenuDNDObserver.__defineGetter__('mObservers', function() {
+    var observers = [].concat(BookmarksMenuDNDObserver_mObservers.call(this));
+    var items = ['compact-bk-menubar', 'compact-bk-button'];
+    for each (var item in items) {
+      var parent = document.getElementById(item);
+      var popup = parent && parent.getElementsByTagName('menupopup')[0];
+      if (popup) observers.push(popup);
+    }
+    return observers;
+  });
+
+  this.hookFunction('BookmarksMenuDNDObserver.onDragStart',
+      'target\.id == "bookmarks-menu"',
+      'target.id == "compact-bk-button" || $&');
+  this.hookFunction('BookmarksMenuDNDObserver.canDrop',
+      'target\.id == "bookmarks-menu"',
+      'target.id == "compact-bk-button" || $&');
+  this.hookFunction('BookmarksMenuDNDObserver.onDragCloseTarget',
+      'this\.mObservers\[i\]\.parentNode.id == "bookmarks-menu"',
+      'this.mObservers[i].parentNode.id == "compact-bk-button" || $&');
+
+  this.hookFunction('BookmarksMenu.getBTSelection',
+      'case "bookmarks-menu":',
+      'case "compact-bk-button":$&');
+  this.hookFunction('BookmarksMenu.getBTTarget',
+      'case "bookmarks-menu":',
+      'case "compact-bk-button":$&');
+  this.hookFunction('BookmarksMenu.getBTContainer',
+      'case "bookmarks-menu":',
+      'case "compact-bk-button":$&');
+},
+
+initBookmarksFunctions_Fx3: function() {
+  this.hookFunction('BookmarksMenuDropHandler.getSupportedFlavours',
+      /{.*}/m,
+      '{ return CompactBookmarks.getSupportedFlavours(); }');
+  this.hookFunction('PlacesMenuDNDController._openBookmarksMenu',
+      'event\.target\.id == "bookmarksMenu"',
+      'event.target.id == "compact-bk-button" || $&');
+},
+
+initBookmarksItems: function() {
+  this.initBookmarksMenubar();
+  this.initBookmarksButton();
 },
 
 initBookmarksMenubar: function() {
   var compactBookmarksMenubar = document.getElementById('compact-bk-menubar');
   if (!compactBookmarksMenubar || compactBookmarksMenubar.initialized) return;
+  this.c_dump('initBookmarksMenubar');
 
-  var bookmarksMenu = document.getElementById('bookmarks-menu');
-  var compactBookmarksMenu = compactBookmarksMenubar.firstChild;
-  if (bookmarksMenu == compactBookmarksMenu) {
-    bookmarksMenu = document.getElementById('bookmarksMenu');
-    compactBookmarksMenu.style.display = 'none';
-    compactBookmarksMenu = compactBookmarksMenu.nextSibling;
-    compactBookmarksMenu.style.display = '';
-  }
-  this.bookmarksMenuNodes = bookmarksMenu.firstChild.childNodes;
-
-  this.cloneBookmarksMenu(compactBookmarksMenu);
+  compactBookmarksMenubar.removeChild(compactBookmarksMenubar.firstChild);
+  var menu = this.getBookmarksMenu().cloneNode(false);
+  this.cloneBookmarksMenu(menu);
+  compactBookmarksMenubar.appendChild(menu);
+  menu.removeAttribute('disabled');
 
   compactBookmarksMenubar.initialized = true;
 },
 
-cloneBookmarksMenu: function(menu) {
-  if (!menu) return;
-  var popup = menu.getElementsByTagName('menupopup')[0];
-  var separator = popup.firstChild;
-  if ('bookmarksMenuSeparator' != separator.id) return;
+initBookmarksButton: function() {
+  var compactBookmarksButton = document.getElementById('compact-bk-button');
+  if (!compactBookmarksButton || compactBookmarksButton.initialized) return;
+  this.c_dump('initBookmarksButton');
 
-  var nodes = this.bookmarksMenuNodes;
-  for (var i = 0; i < nodes.length && 'menuseparator' != nodes[i].tagName; ++i) {
-    var node = nodes[i];
-    var item = this.cloneNode(node);
-    popup.insertBefore(item, separator);
+  var attrs = this.getBookmarksMenu().attributes;
+  for (var i = 0; i < attrs.length; ++i) {
+    var attr = attrs[i];
+    if (/^on|^(datasources|ref|flags|template|infer)$/.test(attr.name))
+      compactBookmarksButton.setAttribute(attr.name, attr.value);
   }
-  var compactBookmarksToolbarFolderMenu = separator.nextSibling;
-  if (compactBookmarksToolbarFolderMenu) {
-    var bms = PlacesUtils.bookmarks;
-    compactBookmarksToolbarFolderMenu.label = bms.getItemTitle(bms.toolbarFolder);
-    if (PlacesUtils.getQueryStringForFolder) {
-      compactBookmarksToolbarFolderMenu.firstChild.place =
-        PlacesUtils.getQueryStringForFolder(bms.toolbarFolder);
+  this.cloneBookmarksMenu(compactBookmarksButton);
+
+  compactBookmarksButton.initialized = true;
+},
+
+// element manipulate methods
+
+getBookmarksMenu: function() {
+  var fx2Menu = document.getElementById('bookmarks-menu');
+  var fx3Menu = document.getElementById('bookmarksMenu');
+  return fx2Menu ? fx2Menu : fx3Menu;
+},
+
+getBookmarksMenuPopup: function() {
+  return this.getBookmarksMenu().getElementsByTagName('menupopup')[0];
+},
+
+getSupportedFlavours: function() {
+  var bookmarksMenuPopupId = this.getBookmarksMenuPopup().id;
+  var menupopups = document.getElementsByTagName('menupopup');
+  for (var i = 0; i < menupopups.length; ++i) {
+    var menupopup = menupopups[i];
+    if (menupopup.id == bookmarksMenuPopupId && menupopup.getSupportedFlavours) {
+      return menupopup.getSupportedFlavours();
     }
   }
+  return [];
 },
 
-cloneNode: function(node) {
-  var item = document.createElement(node.tagName);
-  for (var i = 0; i < node.attributes.length; ++i) {
-    var attr = node.attributes[i];
-    item.setAttribute(attr.name, attr.value);
+cloneBookmarksMenu: function(parent) {
+  if (!parent || parent.firstChild) return;
+
+  var menupopup = this.getBookmarksMenuPopup();
+  var clone_menupopup = menupopup.cloneNode(false);
+  parent.appendChild(clone_menupopup);
+
+  var nodes = menupopup.childNodes;
+  for (var i = 0; i < nodes.length; ++i) {
+    var node = nodes[i];
+    clone_menupopup.appendChild(node.cloneNode(false));
+    if ('menuseparator' == node.tagName) break;
   }
-  return item;
+
+  var tbFolderMenu = document.getElementById('bookmarksToolbarFolderMenu');
+  if (tbFolderMenu) {
+    var tbFolderPopup = document.getElementById('bookmarksToolbarFolderPopup');
+    var clone_tbFolderMenu = tbFolderMenu.cloneNode(false);
+    var clone_tbFolderPopup = tbFolderPopup.cloneNode(false);
+    var clone_separator = tbFolderMenu.nextSibling.cloneNode(false);
+    clone_tbFolderMenu.appendChild(clone_tbFolderPopup);
+    clone_menupopup.appendChild(clone_tbFolderMenu);
+    clone_menupopup.appendChild(clone_separator);
+  }
 },
 
-// handle events
+// event methods
 
 handleEvent: function(event) {
   switch (event.type) {
     case 'load'  : this.init(); break;
     case 'unload': this.destroy(); break;
-    case 'focus' : this.initBookmarksMenubar(); break;
+    case 'focus' : this.initBookmarksItems(); break;
   }
 }
 
