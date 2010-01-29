656bd1f42414f1e38aad9819fa4e4981d424e11d
Milly <milly.ca@gmail.com>
2010-01-29 19:09:53 +0900

    Add default pref 'compact.menu.icon.multiple'

--- defaults/preferences/compact.js
+++ defaults/preferences/compact.js
@@ -2,3 +2,4 @@ pref("extensions.{57068FBE-1506-42ee-AB02-BD183E7999E4}.description", "chrome://
 pref("compact.menu.icon.enabled", false);
 pref("compact.menu.icon.file", "");
 pref("compact.menu.icon.localfilename", "");
+pref("compact.menu.icon.multiple", false);
